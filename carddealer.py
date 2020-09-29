2b0d7f66e9b22d01e3a16ffc5378dbfb722c70fa
mogomez309 <71673351+mogomez309@users.noreply.github.com>
2020-09-28 21:20:27 -0700

    Add files via upload

---
+++ carddealer.py
@@ -0,0 +1,15 @@
+import random
+
+
+def dealer():
+    card = random.randint(1,13)
+    deck = random.shuffle("A2345678910KQJ")
+    hand = ""
+    for i in range(len(deck)):
+        if card == 1:
+            card = "A"
+            hand = hand + "A"
+        elif card == 11:
+            card = "K"
+            hand = hand + "K"
+
