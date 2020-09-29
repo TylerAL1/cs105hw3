8cb918459f8c0c61121a9cac2b4005f0ba5b6abd
mogomez309 <71673351+mogomez309@users.noreply.github.com>
2020-09-28 21:44:40 -0700

    Add files via upload

---
+++ moisesnapshot.py
@@ -0,0 +1,19 @@
+import random
+
+
+def dealer():
+    card = random.randint(1,13)
+    deck = random.shuffle("A2345678910KQJ")
+    hand = ""
+    for i in range(len(deck)):
+        if card == 1:
+            card = "A"
+            hand = hand + "A"
+        elif card == 11:
+            card = "K"
+            hand = hand + "K"
+
+# Yeah whatever. I'm just going to add gibberish
+
+def whatever(oompaloompa):
+    Wonka = "nonsense"
